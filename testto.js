6fe814233e5137ff2109dd609850688fd3b537ec
shahinmahmudov <ifastsahin@gmail.com>
2023-03-06 09:57:45 +0400

    testto.js and testto2 created

---
+++ testto.js
@@ -0,0 +1,8 @@
+let num = 5;
+let sum = 1;
+
+for(let i = 1; i <= num; i++) {
+    sum *= i;
+    console.log(sum)
+    
+}
---
+++ testto2.js
@@ -0,0 +1,9 @@
+let num = 5;
+let sum = 1;
+let a = 1;
+
+while (a <= num) {
+  sum *= a;
+  a++;
+}
+console.log(sum);
